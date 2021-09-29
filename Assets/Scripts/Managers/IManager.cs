d36362a7923170c5f929fbd19117ef2cabe96f8a
matthew st-germain <mgermain@isimtl.local>
2021-09-29 10:08:40 -0400

    Whoops
    Pushed a compiler error

--- Assets/Scripts/Managers/IManager.cs
+++ Assets/Scripts/Managers/IManager.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IManager : MonoBehaviour
+public interface IManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void AppInitialize();
+    void AppSecondInitialize();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
